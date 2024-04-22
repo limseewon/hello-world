7e25845a6d2fd3729464bace6de11f037698636f
jun <emfowkd@gmail.com>
2024-04-22 23:14:42 +0900

    sql setting

---
+++ admin/dbcon.php
@@ -0,0 +1,13 @@
+<?php
+ $hostname = 'localhost';
+ $dbuserid = 'worldhello ';
+ $dbpasswd = '';
+ $dbname = '';
+
+ $mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
+
+ if ($mysqli->connect_errno) {
+ die('mysqli connection error: ' . $mysqli->connect_error);
+ } 
+
+?>
