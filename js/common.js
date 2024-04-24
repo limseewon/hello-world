a5732f18491d550749b05d734544f914b1ee991b
junb119 <emfowkd@gmail.com>
2024-04-24 15:52:35 +0900

    파일[수정] header.html

--- js/common.js
+++ js/common.js
@@ -1,1 +1,11 @@
-$('#header').load('./header.html', setHeader);
+$(function () {
+  $('.main_wrapper').load('./header.html');
+  let documentHeight = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight
+  );
+  document.querySelector('header').style.height = documentHeight + 'px';
+});
