66271859a5055becdb16c8b16e8b11236f3ad82a
coolcool <159146271+limseewon@users.noreply.github.com>
2024-05-01 16:55:00 +0900

    add

--- admin/inc/admin_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//관리자 검사
-if (!isset($_SESSION['AUID'])) {
-  echo "<script>
-    alert('권한이 없습니다.');
-    location.href='/helloworld/admin/login.php';
-  </script>";
-}
--- coupon/image_del.php
+++
@@ -1,7 +0,0 @@
-<?php
-$prevImg = $_POST['prevImg']??'';
-$prevImgDel = $_SERVER['DOCUMENT_ROOT'].$prevImg;
-unlink($prevImgDel);
-echo json_encode($prevImgDel);
-exit;
-?>
