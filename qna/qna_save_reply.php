6d3736996c772b90f449306561fdb6ee1080d965
coolcool <159146271+limseewon@users.noreply.github.com>
2024-05-02 11:59:53 +0900

    기능[추가] 댓글 저장 수정

--- qna/qna_save_reply.php
+++ qna/qna_save_reply.php
@@ -4,14 +4,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/helloworld/inc/dbcon.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $qna_id = $_POST['id'];
     $comment = $_POST['comment'];
-    
+    $name = $_POST['name'];
+    $regdate = date('Y-m-d H:i:s'); // 현재 시간을 regdate로 설정
+
     // 댓글 저장 쿼리 실행
-    $sql = "INSERT INTO qna_comment (qna_id, comment) VALUES ('$qna_id', '$comment')";
-    
+    $sql = "INSERT INTO qna_comment (qna_id, comment, name, regdate) VALUES ('$qna_id', '$comment', '$name', '$regdate')";
     if ($mysqli->query($sql) === TRUE) {
         echo "success";
     } else {
-        echo "error";
+        echo "error: " . $mysqli->error;
     }
 }
 ?>
