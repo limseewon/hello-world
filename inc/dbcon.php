23eeafe82d6b496d6e8568128a6a669f05696e73
jun <emfowkd@gmail.com>
2024-05-11 16:31:24 +0900

    기능[추가] 로그인 html 레이아웃

--- inc/dbcon.php
+++ inc/dbcon.php
@@ -3,7 +3,7 @@
  $dbuserid = 'worldhello';
  $dbpasswd = 'sudo0212!';
  $dbname = 'worldhello';
-
+ 
  $mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
 
  if ($mysqli->connect_errno) {
