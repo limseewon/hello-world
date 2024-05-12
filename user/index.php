77fa0c6b395469d857f469bf6e3a68499ca2c01f
wonsc12 <turnapple123@naver.com>
2024-05-12 17:51:40 +0900

    js css[추가]index-section1슬라이드

--- user/index.php
+++ user/index.php
@@ -1,20 +1,40 @@
+
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css" integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw==" crossorigin="anonymous" referrerpolicy="no-referrer"> <!--jquery ui-->
+    <link rel="stylesheet" href="/helloworld/user/css/common.css"/>
+    <link rel="stylesheet" href="/helloworld/user/css/index.css"/>
+    <script src="js/index.js"></script>
 <main>
     <section class="sec1">
       <!-- Swiper -->
       <div class="swiper sec1_slide">
         <div class="swiper-wrapper">
-          <div class="swiper-slide"><img src="" alt="슬라이드 이미지_01"></div>
-          <div class="swiper-slide"><img src="" alt="슬라이드 이미지_02"></div>
-          <div class="swiper-slide"><img src="g" alt="슬라이드 이미지_03"></div>
+          <div class="swiper-slide"><img src="img/index_section1.webp" alt="슬라이드 이미지_01"></div>
+          <div class="swiper-slide"><img src="img/index_section2.png" alt="슬라이드 이미지_02"></div>
+          <div class="swiper-slide"><img src="img/index_section3.png" alt="슬라이드 이미지_03"></div>
+          <div class="swiper-slide"><img src="img/index_section4.gif" alt="슬라이드 이미지_04"></div>
         </div>
         <div class="swiper-pagination"></div>
+        <div class="swiper-button-next se1_next"></div>
+        <div class="swiper-button-prev se1_prev"></div>  
       </div>
+      
+      
     </section>
     <section class="sec2">
       <h2 class="jua dark sec_tt">어떤 강의를 찾고 있나요?</h2>
       <form action="" class="search">
         <label for="course_search" type="hidden"></label>
-        <input type="text" id="course_search" placeholder="필요한 강의를 찾아보세요.">
+        <input type="text" id="course_search" placeholder="배우고 싶은 강의를 입력하세요.">
         <button><i class="ti ti-search"></i></button>
       </form>
       <div class="category_box radius_12">
@@ -65,213 +85,174 @@
       </div>
     </section>
     <section class="sec3 container">
-      <h2 class="">최신강의</h2>
+      <h2 class="jua dark sec_tt">최신 강의</h2>
       <!-- Swiper -->
       <div class="page_wrap">
         <div class="swiper recom_slide">
           <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
+            
+                <div class="swiper-slide">
+                  <div class="card">
+                    <img src="" class="card-img-top" alt="강의 썸네일">
+                    <div class="card-body">
+                      <div class="badge_wrap">
+                        <span class="badge rounded-pill b-pd
+                        ">
+                          
+                        </span>
+                        <span class="badge rounded-pill green_bg b-pd">
+                          
+                        </span>
+                      </div>
+                      <h5 class="card-title">
+                        
+                      </h5>
+                      <div class="card-text">
+                        <p class=""><i class="ti ti-calendar-event"></i>수강기간
+                          <span class="duration">
+                            
+                          </span>
+                        </p>
+                        
+                          <div>
+                            <span class="content_tt red">무료</span>
+                          </div>
+                        
+                      </div>
+                    </div>
                   </div>
-                  <h5 class=""></h5>
-                  <div class="">
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                   
+                  <div class="view_wrap d-flex align-items-center justify-content-center flex-column">
+                    <a href="" class="view_btn">상세보기</a>
+                    <span>
+                      <a href="" class="card_like">
+                        
+                      </a>
+                      <a href="" class="card_cart">
+                        
+                      </a>
+                    </span>
                   </div>
                 </div>
-              </div>
-            </div>
+            
           </div>
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next recom_next"></div>
+        <div class="swiper-button-prev recom_prev"></div>
       </div>
     </section>
-    <section class="sec4 container">
-      <h2 class="">인기강의</h2>
-      <!-- Swiper -->
-      <div class="page_wrap">
-        <div class="swiper recom_slide">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class=""></h5>
-                  <div class="">
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                   
+  <section class="sec4 container">
+    <h2 class="jua dark sec_tt">추천강의</h2>
+    <!-- Swiper -->
+    <div class="page_wrap">
+      <div class="swiper new_slide">
+        <div class="swiper-wrapper">
+         
+              <div class="swiper-slide">
+                <div class="card">
+                  <img src="" class="card-img-top" alt="강의 썸네일">
+                  <div class="card-body">
+                    <div class="badge_wrap">
+                      <span class="badge rounded-pill b-pd
+                      ">
+                        
+                      </span>
+                      <span class="badge rounded-pill green_bg b-pd">
+                        
+                      </span>
+                    </div>
+                    <h5 class="card-title">
+                      
+                    </h5>
+                    <div class="card-text">
+                      <p class=""><i class="ti ti-calendar-event"></i>수강기간
+                        <span class="duration">
+                          
+                        </span>
+                      </p>
+                      
+                      
+                        <div>
+                          <span class="content_tt red">무료</span>
+                        </div>
+                      
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                   
-                  </div>
+                <div class="view_wrap d-flex align-items-center justify-content-center flex-column">
+                  <a href="" class="view_btn">상세보기</a>
+                  <span>
+                    <a href="" class="card_like">
+                      
+                    </a>
+                    <a href="" class="card_cart">
+                      
+                    </a>
+                  </span>
                 </div>
               </div>
-            </div>
-          </div>
+          
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
       </div>
-    </section>
-    <section class="sec5 container">
-      <h2 class="jua dark sec_tt">입문자를 위한 초급강의</h2>
-      <!-- Swiper -->
-      <div class="page_wrap">
-        <div class="swiper new_slide">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class=""></h5>
-                  <div class="">
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src="" class="" alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class=""></h5>
-                  <div class="">
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src=""alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    <span class=""></span>
-                    <span class=""></span>
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                    
+      <div class="swiper-button-next new_next"></div>
+      <div class="swiper-button-prev new_prev"></div>
+    </div>
+  </section>
+  <section class="sec5 container">
+    <h2 class="jua dark sec_tt">입문자를 위한 추천강의</h2>
+    <!-- Swiper -->
+    <div class="page_wrap">
+      <div class="swiper beginner_slide">
+        <div class="swiper-wrapper">
+         
+              <div class="swiper-slide">
+                <div class="card">
+                  <img src="" class="card-img-top" alt="강의 썸네일">
+                  <div class="card-body">
+                    <div class="badge_wrap">
+                      <span class="badge rounded-pill b-pd
+                      ">
+                        
+                      </span>
+                      <span class="badge rounded-pill green_bg b-pd">
+                        
+                      </span>
+                    </div>
+                    <h5 class="card-title">
+                      
+                    </h5>
+                    <div class="card-text">
+                      <p class=""><i class="ti ti-calendar-event"></i>수강기간
+                        <span class="duration">
+                          
+                        </span>
+                      </p>
+                      
+                      
+                        <div>
+                          <span class="content_tt red">무료</span>
+                        </div>
+                      
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="card">
-                <img src=""alt="">
-                <div class="card-body">
-                  <div class="badge_wrap">
-                    
-                  </div>
-                  <h5 class="card-title"></h5>
-                  <div class="card-text">
-                    <p class=""></p>
-                    <p class=""></p>
-                  </div>
+                <div class="view_wrap d-flex align-items-center justify-content-center flex-column">
+                  <a href="" class="view_btn">상세보기</a>
+                  <span>
+                    <a href="" class="card_like">
+                      
+                    </a>
+                    <a href="" class="card_cart">
+                      
+                    </a>
+                  </span>
                 </div>
               </div>
-            </div>
-          </div>
+          
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
       </div>
-    </section>
-    
-    
-    
-    
+      <div class="swiper-button-next new_next"></div>
+      <div class="swiper-button-prev new_prev"></div>
+    </div>
+  </section>
+  
   </main
---
+++ user/js/index.js
@@ -0,0 +1,61 @@
+$(function(){ 
+  var swiper = new Swiper(".sec1_slide", {
+    slidesPerView: 1,
+    speed: 2000,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay : {  
+      delay : 2500,  
+      disableOnInteraction : false, 
+    },
+    navigation: {
+      prevEl: ".se1_prev",
+      nextEl: ".se1_next",
+    },
+    // loopAdditionalSlides: 1
+  });
+
+  // autoplayStart autoplayStop
+  let slide = $(".swiper-slide");
+
+  slide.on("mouseenter", function (e) {
+    swiper.autoplay.stop();
+  });
+  slide.on("mouseleave", function (e) {
+    swiper.autoplay.start();
+  });
+
+  var swiper = new Swiper(".recom_slide", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 2000,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction : false, 
+    },
+    navigation: {
+      prevEl: ".recom_prev",
+      nextEl: ".recom_next",
+    },
+  });
+
+  var swiper = new Swiper(".new_slide", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 2000,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction : false, 
+    },
+    navigation: {
+      prevEl: ".new_prev",
+      nextEl: ".new_next",
+    },
+  });
+});
