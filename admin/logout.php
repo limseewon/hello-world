ab7754da0e6e43f61ffe678a5213c47fa9b4c682
junb119 <emfowkd@gmail.com>
2024-05-13 17:48:57 +0900

    버그수정[미완] 백엔드 로그아웃

--- admin/logout.php
+++ admin/logout.php
@@ -4,5 +4,5 @@ session_unset();
 session_destroy();
 $url = '/helloworld/admin/login.php';
 echo "<script>alert('로그아웃 되었습니다.');</script>"; // 작동 왜안할까
-header("Location:$url");
+header("Location.href:$url");
 die();
