9ddd5ab87f1dc05d50b697cc636595159fcd5235
wonsc12 <jangkipong12@gmail.com>
2024-05-14 14:52:37 +0900

    경로[수정]강의css

--- user/index.php
+++ user/index.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Home';
-$cssRoute1 ='<link rel="stylesheet" href="/helloworld/user/css/index.css"/>';
+$cssRoute1 ='';
 $cssRoute2 ='';
 $script1 = '';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/helloworld/inc/user_header.php';
@@ -52,8 +52,9 @@ while ($rc_rs = $rc_result->fetch_object()) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/helloworld/user/css/common.css"/>
+    <link rel="stylesheet" href="/helloworld/user/css/index.css"/>
     
-    <script src="js/index.js"></script>
 <main>
     <section class="sec1">
       <!-- Swiper -->
@@ -457,3 +458,4 @@ while ($rc_rs = $rc_result->fetch_object()) {
   <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/helloworld/inc/user_footer.php';
 ?>
+<script src="js/index.js"></script>
