158dbaf30c76f310ddbd0a52e80bcb23dccbb628
coolcool <seewon3342@gmail.com>
2024-05-15 20:30:07 +0900

    기능[추가,수정] 질문 등록 구현

--- user/qna/qna_write_ok.php
+++ user/qna/qna_write_ok.php
@@ -1,21 +1,31 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/helloworld/inc/dbcon.php';
 
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     $title = $_POST['title'];
+//     $cid = isset($_POST['cid']) ? $_POST['cid'] : '';
+//     $lecture_name = isset($_POST['lecture_name']) ? $_POST['lecture_name'] : '';
+//     $content = isset($_POST['content']) ? urldecode($_POST['content']) : '';
+//     $name = isset($_POST['name']) ? $_POST['name'] : '';
+//     $reply = '미답변';
+//     $file = $_FILES['files']['name'];
+//     $date = date("Y-m-d H:i:s");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $title = $_POST['title'];
-    $cid = $_POST['cid'];
-    $lecture_name = $_POST['lecture_name'];
-    $content = urldecode($_POST['content']);
-    $name = $_POST['name']; // 임시로 관리자 이름 사용
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $cid = $mysqli->real_escape_string($_POST['cid']);
+    $lecture_name = $mysqli->real_escape_string($_POST['lecture_name']); // 이 부분 추가
+    $content = $mysqli->real_escape_string(urldecode($_POST['contents']));
+    $name = '사용자 이름'; // 실제 사용자 이름으로 변경해야 합니다.
     $reply = '미답변';
-    $file = $_FILES['files']['name'];
+    $file = $mysqli->real_escape_string($_FILES['file']['name']);
 
     // 파일 업로드 처리
     $upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/helloworld/user/uploads/';
     $upload_file = $upload_dir . basename($file);
     move_uploaded_file($_FILES['file']['tmp_name'], $upload_file);
 
-    $sql = "INSERT INTO qna (name, title, cid ,lecture_name, content, reply, files, regdate) VALUES ('$name', '$title', '$cid','$lecture_name', '$content', '$reply', '$file', NOW())";
+    $sql = "INSERT INTO qna (name, title, cid ,lecture_name, content, reply, files, date) VALUES ('$name', '$title', '$cid','$lecture_name', '$content', '$reply', '$file', NOW())";
     $result = $mysqli->query($sql);
 
     if ($result) {
