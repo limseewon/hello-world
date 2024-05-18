c2a0a45188d185def855ddc924040eb0aa98c298
coolcool <159146271+limseewon@users.noreply.github.com>
2024-05-19 00:58:10 +0900

    기능[추가] 댓글 시간 표시

--- user/qna/qna_save_insert.php
+++ user/qna/qna_save_insert.php
@@ -17,13 +17,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $name = $member['username'];
 
         // 댓글 저장
-        $sql = "INSERT INTO qna_comment (idx, name, comment) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO qna_comment (idx, name, comment, regdate) VALUES (?, ?, ?, NOW())";
         $stmt = $mysqli->prepare($sql);
         $stmt->bind_param("iss", $idx, $name, $comment);
         $comment_result = $stmt->execute();
 
-        // 댓글 등록 성공 시 qna 테이블의 reply 컬럼은 업데이트하지 않음
+        // 댓글 등록 성공 시 qna 테이블의 reply 컬럼 업데이트
         if ($comment_result) {
+            $sql = "UPDATE qna SET reply = '답변' WHERE idx = '$idx'";
+            $update_result = $mysqli->query($sql);
+
             echo "<script> alert('댓글이 등록되었습니다.'); location.href = 'qna_detail.php?id=$idx'; </script>";
         } else {
             echo "<script> alert('댓글 등록에 실패했습니다.'); history.back(); </script>";
