5271533adcd4a8526dae3d0dc719f6b7d77c97d1
jun <emfowkd@gmail.com>
2024-05-18 22:35:45 +0900

    기능[추가] ordered_courses 기한 컬럼 코드 추가

--- user/class/courses_ok.php
+++ user/class/courses_ok.php
@@ -15,12 +15,23 @@ $totalprice = $_POST['total']??null; // POST 요청에서 총 가격(totalprice)
 
 // URL에서 강의 ID를 가져옴
 $cid = $_REQUEST['cid'];
-$sql = "SELECT price FROM courses WHERE cid = $cid"; // SQL 쿼리를 사용하여 해당 강의 ID에 해당하는 강의의 가격을 가져옴
+$sql = "SELECT price,due FROM courses WHERE cid = $cid"; // SQL 쿼리를 사용하여 해당 강의 ID에 해당하는 강의의 가격을 가져옴
 // echo $sql;
 $result = $mysqli->query($sql);
 $row = $result->fetch_object();
 $price = $row->price;
 $total = isset($totalprice) ? $totalprice : $price; // 총 가격(totalprice)이 설정되어 있는지 확인  설정되어 있으면 해당 값을 사용 그렇지 않으면 강의의 가격을 사용하여 총 가격을 설정
+
+$due = $row->due;
+echo 'due : '.$due;
+if ($due =='무제한') {
+  $due = 9999;
+} else {
+  $due = preg_replace('/[^0-9]/', '', $due);
+}
+$date = new DateTime();
+$date->modify('+'.$due.' month'); //현재 날짜의 n달 후 기한 구하기
+$limit = $date->format('Y-m-d');
 
 
 // 세션에 UID가 있는지 확인
@@ -32,7 +43,7 @@ if(isset($_SESSION['UID'])) {
 
   // 이미 주문된 강의가 없으면, 새로운 주문을 추가
   if(!$rscuc2) {
-    $sql = "INSERT INTO ordered_courses (course_id, member_id ,total_price) VALUES ({$cid}, '{$member_id}',{$total})";
+    $sql = "INSERT INTO ordered_courses (course_id, member_id ,progress, satisfaction, regdate, total_price ,use_max_date) VALUES ({$cid}, '{$member_id}', 0, '4.3',CURDATE() ,{$total},'{$limit}')";
     // echo $sql;
     $result = $mysqli->query($sql);
   
